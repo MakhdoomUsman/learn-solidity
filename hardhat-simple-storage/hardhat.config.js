96ce69416e4323a81ed2353f1318efd9dfe3d3bc
MakhdoomUsman <humanozman9@gmail.com>
2024-05-02 19:08:24 +0500

    working on hardhat contract

--- hardhat-simple-storage/hardhat.config.js
+++ hardhat-simple-storage/hardhat.config.js
@@ -1,6 +1,22 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require("dotenv").config();
+require("@nomicfoundation/hardhat-verify");
 /** @type import('hardhat/config').HardhatUserConfig */
+const SEPOLIA_RCP_URL = process.env.SEPOLIA_RCP_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 module.exports = {
+
+  defaultNetwork: "hardhat",
+  networks: {
+    sepolia: {
+      url: SEPOLIA_RCP_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 11155111
+    }
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY
+  },
   solidity: "0.8.23",
 };
--- hardhat-simple-storage/scripts/deploy.js
+++ hardhat-simple-storage/scripts/deploy.js
@@ -1,4 +1,4 @@
-const { ethers } = require("hardhat")
+const { ethers, run, network } = require("hardhat")
 
 async function main() {
     const SimpleStorageFacotry = await ethers.getContractFactory(
@@ -6,7 +6,26 @@ async function main() {
     )
     console.log("Deploying!!")
     const simpleStorage = await SimpleStorageFacotry.deploy()
-    await simpleStorage.deployed()
+    console.log(`'Deployed ', ${simpleStorage.target}`);
+    console.log("network", network.config);
+
+    if (network.config.chainId === 11155111 && process.env.ETHERSCAN_API_KEY) {
+        await simpleStorage.deploymentTransaction().wait(6);
+        await verify(simpleStorage.target, [])
+    }
+    // await simpleStorage.deployed()
+}
+async function verify(contractAddress, args) {
+    console.log("Verifies hardhat!!!");
+    try {
+        await run("verify:verify", {
+            address: contractAddress,
+            contructorArguments: args
+        })
+    } catch (error) {
+        if (error.message.toLowerCase().includes("already verified")) console.log("Already Verified!");
+        console.log("Error!", error);
+    }
 }
 
 main()
